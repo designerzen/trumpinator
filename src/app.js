fac82c7f2429ba8d209967c21759b49a3e21b2ae
designerzen <designerzen@gmail.com>
2017-01-09 23:29:44 +0000

    Added image references

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@
 
 // Create our build for this project...
 import Polyfiller from 'babel-polyfill';
+
 import markup from './markup/index.html';
 import styles from './style/style.css';
-import scripts from './script/main';
+import scripts from './script/index.js';
 
 console.log("Loading Trumpinator");
